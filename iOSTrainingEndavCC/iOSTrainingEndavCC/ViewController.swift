212828be09e2a79d7df16091bc7938299c41d9c6
Andres Diaz <andres.diaz@getbuilt.com>
2020-08-20 20:10:48 -0500

    1st attempt to layout

---
+++ iOSTrainingEndavCC/iOSTrainingEndavCC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTrainingEndavCC
+//
+//  Created by Andres Diaz on 8/20/20.
+//  Copyright © 2020 Andres Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
